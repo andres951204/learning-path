1a263911692169d4c7a49ea7555dd3447425cc1a
Andres acevedo <andresca6@gmail.com>
2024-01-31 12:05:38 -0500

    feat: flyweight exercise

---
+++ JavaScript Design Patterns/flyweight/exercise.js
@@ -0,0 +1,45 @@
+/**
+ * Flyweight Coding Exercise
+You are given a class called Sentence ,
+ which takes a string such as 'hello world'. 
+ You need to provide a method at(index) such that the method returns a flyweight that can be used to capitalize a 
+ particular word in the sentence.
+Typical use would be something like:
+let s = new Sentence('alpha beta gamma');
+s.at(1).capitalize = true;
+console.log(s.toString()); // alpha BETA gamma
+ */
+
+class Sentence {
+    constructor(words) {
+        this.words = words.split(' ');
+        this.tokens = {};
+    }
+
+    at(index) {
+        if (this.tokens[index] === undefined) {
+            this.tokens[index] = new WordToken();
+        }
+        return this.tokens[index];
+    }
+
+    toString() {
+        let result = [];
+        for (let i = 0; i < this.words.length; i++) {
+            let word = this.words[i];
+            let token = this.tokens[i];
+            if (token && token.capitalize) {
+                result.push(word.toUpperCase());
+            } else {
+                result.push(word);
+            }
+        }
+        return result.join(' ');
+    }
+}
+
+class WordToken {
+    constructor(capitalize = false) {
+        this.capitalize = capitalize;
+    }
+}
